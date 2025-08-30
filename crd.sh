f64477a94a0cbfaa4b2c249eb399df77851202e9
aLpHa-Git-69 <glgaming7075@gmail.com>
2025-08-30 12:36:29 +0530

    rise

--- crd.sh
+++ crd.sh
@@ -1,23 +1,23 @@
 # repo init rom
-repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs
-
+repo init -u https://github.com/RisingOS-Revived/android -b sixteen-los --git-lfs
 #linux-x86 remove
 rm -rf prebuilts/clang/host/linux-x86
 
-#sync 
+#sync
 /opt/crave/resync.sh
 
 # clone dt
 git clone https://github.com/aLpHa-Git-69/android_device_xiaomi_lavender.git -b derp device/xiaomi/lavender
 
 #sign
-curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh | bash
+#curl -sSf https://raw.githubusercontent.com/Trijal08/crDroid-build-signed-script-auto/main/create-signed-env.sh | bash
+gk -f
 
 # Setup the bubuilenvironment
 . build/envsetup.sh
 
 # Lunch
-lunch lineage_lavender-bp2a-userdebug
+riseup lavender userdebug
 
 # Build
-mka derp
+rise sb
