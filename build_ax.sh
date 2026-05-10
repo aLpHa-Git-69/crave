611c30ace067a7bd8a0f4a31440bc9bcc88f0618
_aLpHa <94362542+aLpHa-Git-69@users.noreply.github.com>
2026-05-10 19:40:09 +0530

    changed script

--- build_ax.sh
+++ build_ax.sh
@@ -25,7 +25,13 @@ echo "------------------------"
 
 # Dt
 
-git clone https://github.com/Never-Alive/device_xiaomi_haydn -b axion device/xiaomi/haydn
+git clone https://github.com/aLpHa-Git-69/device_xiaomi_lavender -b Axion device/xiaomi/lavender
+git clone https://github.com/Amritorock/vendor_xiaomi_lavender vendor/xiaomi/lavender
+git clone https://github.com/Amritorock/kernel_xiaomi_lavender -b android-16 kernel/xiaomi/lavender --depth=1
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone https://github.com/Amritorock/hardware_dolby hardware/dolby
+git clone https://github.com/Amritorock/hardware_qcom_camera hardware/qcom-caf/sdm660/camera
+
 
 echo "---------------------"
 echo "Tress clone completed"
@@ -49,7 +55,7 @@ echo "----------------------------"
 
 # boss
 
-axion haydn va
+axion lavender va
 
 ax -br
 echo "----------"
