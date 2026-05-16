8981d7e6d73aa570586dffc6d03f24b4e47825d6
_aLpHa <94362542+aLpHa-Git-69@users.noreply.github.com>
2026-05-16 16:06:32 +0530

    Nothing

---
+++ rn.sh
@@ -0,0 +1,29 @@
+/opt/crave/resync.sh
+
+/opt/crave/resync.sh
+
+/opt/crave/resync.sh
+
+. b*/e*
+
+echo "---------------------------"
+echo "Build/envsetup.sh completed"
+echo "---------------------------"
+
+# signing script
+
+gk -s
+
+echo "----------------------------"
+echo "Rom signed with private keys"
+echo "----------------------------"
+
+# boss
+
+axion camellia va
+
+ax -br
+
+echo "----------"
+echo "Done"
+echo "----------"
