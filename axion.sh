9f589d362ea9cc408f3176c83aed8116db694cae
_aLpHa <94362542+aLpHa-Git-69@users.noreply.github.com>
2026-05-23 17:44:12 +0530

    Update git clone commands in axion.sh

--- axion.sh
+++ axion.sh
@@ -25,10 +25,10 @@ echo "------------------------"
 
 # Dt
 
-git clone https://github.com/aLpHa-Git-69/device_xiaomi_camellia.git -b exp device/xiaomi/camellia
-git clone https://github.com/aLpHa-Git-69/android_vendor_xiaomi_camellia.git vendor/xiaomi/camellia
+git clone https://github.com/aLpHa-Git-69/device_xiaomi_camellia.git -b axion device/xiaomi/camellia
+git clone https://github.com/aLpHa-Git-69/vendor_xiaomi_camellia.git vendor/xiaomi/camellia
 git clone https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git device/xiaomi/camellia-kernel
-git clone https://github.com/xiaomi-mediatek-devs/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
 git clone https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
