9cdf3ec1e9c487ca451779beda790e6bfe7422b0
_aLpHa <94362542+aLpHa-Git-69@users.noreply.github.com>
2026-05-23 17:52:31 +0530

    Fix repository URLs in inf.sh script

--- inf.sh
+++ inf.sh
@@ -26,9 +26,9 @@ echo "------------------------"
 # Dt
 
 git clone https://github.com/aLpHa-Git-69/device_xiaomi_camellia.git -b infinityx device/xiaomi/camellia
-git clone https://github.com/aLpHa-Git-69/vendor-xiaomi-camellia.git vendor/xiaomi/camellia
+git clone https://github.com/aLpHa-Git-69/vendor_xiaomi_camellia.git vendor/xiaomi/camellia
 git clone https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git device/xiaomi/camellia-kernel
-git clone https://github.com/xiaomi-mediatek-devs/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
 git clone https://github.com/LineageOS/android_hardware_mediatek.git hardware/mediatek
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
